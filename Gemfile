1fe190b0315d602e4ae562433f312cfec7db3315
Normar Macawile <nmacawile@gmail.com>
2024-03-15 12:05:59 +0000

    Create the User model

--- Gemfile
+++ Gemfile
@@ -37,6 +37,8 @@ gem "bootsnap", require: false
 
 gem "faker"
 
+gem "devise"
+
 group :development, :test do
   # See https://guides.rubyonrails.org/debugging_rails_applications.html#debugging-with-the-debug-gem
   gem "debug", platforms: %i[ mri windows ] 
---
+++ spec/models/user_spec.rb
@@ -0,0 +1,15 @@
+require 'rails_helper'
+
+RSpec.describe User, type: :model do
+  it { is_expected.to validate_presence_of :first_name }
+  it { is_expected.to validate_presence_of :last_name }
+
+  describe "#full_name" do
+    let(:first_name) { subject.first_name }
+    let(:last_name) { subject.last_name }
+
+    it "returns the full name" do
+      expect(subject.full_name).to eq "#{first_name} #{last_name}"
+    end
+  end
+end
