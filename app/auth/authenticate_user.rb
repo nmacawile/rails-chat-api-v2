9d6dd0dbbcb57c86e05f1ebec9eb1532aa7ec67a
Normar Macawile <nmacawile@gmail.com>
2024-03-17 15:15:16 +0000

    Refactor user authenticator

--- app/auth/authenticate_user.rb
+++ app/auth/authenticate_user.rb
@@ -6,11 +6,8 @@ class AuthenticateUser
 
   def call
     if user&.valid_password?(password)
-      return { auth_token: JwtCodec.encode({ user_id: user.id }),
-               user: user.slice(:first_name,
-                                :last_name,
-                                :full_name,
-                                :email) }
+      return { auth_token: auth_token,
+               user: user.data }
     end
     raise ExceptionHandler::AuthenticationError,
           "Invalid credentials."
@@ -19,6 +16,10 @@ class AuthenticateUser
   private
 
   attr_reader :email, :password
+
+  def auth_token
+    JwtCodec.encode({ user_id: user.id })
+  end
 
   def user
     @user ||= User.find_by_email(email)
