58e7d60b7b7dee3a247dcf76ba6e875361d768b9
Normar Macawile <nmacawile@gmail.com>
2024-04-29 17:14:06 +0000

    Use denormalized query to find user chats

--- app/controllers/api/v1/chats_controller.rb
+++ app/controllers/api/v1/chats_controller.rb
@@ -3,20 +3,10 @@ class Api::V1::ChatsController < ApplicationController
   before_action :restrict_access, only: :show
 
   def index
-    user_chat_ids = current_user.chats.select(:id)
-    # query latest message time by distinct chats, latest first
-    latest_message_times_by_distinct_chats_subquery = Message
-      .select("messageable_id, MAX(created_at) AS latest_message_time")
-      .where(messageable_id: user_chat_ids)
-      .order("latest_message_time DESC")
-      .group(:messageable_id).to_sql
-
-    # query latest chat messages, eager load chat and chat users
-    @latest_chat_messages_with_inclusions = Message
-        .joins("JOIN (#{latest_message_times_by_distinct_chats_subquery}) AS latest_message_times " \
-              "ON messages.messageable_id = latest_message_times.messageable_id " \
-              "AND messages.created_at = latest_message_times.latest_message_time")
-        .includes(messageable: :users)
+    @chats = current_user.chats
+               .includes(:latest_message, :users)
+               .where.not(latest_message: nil)
+               .order(updated_at: :desc)
   end
 
   def show
